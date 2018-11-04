8b5ee2779e6df95389cda6fed377d48964895432
Wille Houm <willem.houm@gmail.com>
2018-11-04 15:59:37 +0100

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -9,10 +9,6 @@ export default new Router({
   routes: [
     {
       path: "/",
-      redirect: "/index"
-    },
-    {
-      path: "/index",
       name: "Index",
       component: Index
     },
